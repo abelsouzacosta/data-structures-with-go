d0dedea7cde447116272f266e492251b06a54dcc
abelsouzacosta <abelsouzacostajunior@proton.me>
2025-10-14 19:23:17 -0300

    [chore]: InsertAtHead
    This function will insert a node at the head of a given linked list,
    since when we create the linked list we start the values of head and
    tail as nil, when we are creating the first node we attribute the value
    of the next reference as the current head of the list
    If the tail is also nil, then the list was empty, in this case the tail
    will be also the node inserted

--- src/data_structures/linked_list/linked_list.go
+++ src/data_structures/linked_list/linked_list.go
@@ -13,3 +13,22 @@ func (list *LinkedList) IsEmpty() bool {
 	return list.head == nil
 }
 
+/*
+* InsertAtHead will insert a element in the head of the list
+*
+* Creates a node that will be initialized with the data provided
+* and sets the next reference of the node the the current head
+* since the list is created having nil as the default value
+* for head if the list is empty then the first element inserted
+* will have nil in the next reference
+ */
+func (list *LinkedList) InsertAtHead(data int) {
+	node := &Node{data: data, next: list.head}
+	list.head = node
+
+	// if there's no tail, then the list is empty
+	// and the first node is the last node
+	if list.tail == nil {
+		list.tail = node
+	}
+}
