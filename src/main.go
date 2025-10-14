760f34a365fafebf9ced3e4e674b69816be8e639
abelsouzacosta <abelsouzacostajunior@proton.me>
2025-10-14 17:00:12 -0300

    [chore]: Stack

--- src/main.go
+++ src/main.go
@@ -7,17 +7,34 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello World")
 	s := stack.New()
 
 	s.Push(2)
 	s.Push(3)
-	top, err := s.Peek()
+	s.Push(15)
+	s.Push(33)
+	s.Push(30)
 
-	if err == nil {
+	top, errTop := s.Peek()
+	max, errMax := s.Max()
+	min, errMin := s.Min()
+
+	if errTop == nil {
 		fmt.Println("The top value is: ", top)
 	} else {
-		fmt.Println("error: ", err)
+		fmt.Println("error: ", errTop)
+	}
+
+	if errMax == nil {
+		fmt.Println("The max value is: ", max)
+	} else {
+		fmt.Println("error", errMax)
+	}
+
+	if errMin == nil {
+		fmt.Println("The min value is: ", min)
+	} else {
+		fmt.Println("error", errMin)
 	}
 
 	s.Print()
