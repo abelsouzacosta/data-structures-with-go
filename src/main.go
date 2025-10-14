f0d6e4da79cf70adf058b9a3d5be6f43d459b87c
abelsouzacosta <abelsouzacostajunior@proton.me>
2025-10-14 16:42:53 -0300

    [feat]: Stack
    Testing my stack implementation

--- src/main.go
+++ src/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/abelsouzacosta/data-structures-with-go/src/data_structures/stack"
+)
 
 func main() {
 	fmt.Println("Hello World")
+	s := stack.New()
+
+	s.Push(2)
+	s.Push(3)
+	top, err := s.Peek()
+
+	if err == nil {
+		fmt.Println("The top value is: ", top)
+	} else {
+		fmt.Println("error: ", err)
+	}
+
+	s.Print()
 }
