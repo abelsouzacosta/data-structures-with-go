c9ee08921649d03bc2161bc58c0ed60e63fcdb44
abelsouzacosta <abelsouzacostajunior@proton.me>
2025-10-15 18:30:13 -0300

    [chore]: Binary Search

--- src/main.go
+++ src/main.go
@@ -1,6 +1,11 @@
 package main
 
-import linkedlist "github.com/abelsouzacosta/data-structures-with-go/src/data_structures/linked_list"
+import (
+	"fmt"
+
+	bs "github.com/abelsouzacosta/data-structures-with-go/src/algorithms/binary_search"
+	linkedlist "github.com/abelsouzacosta/data-structures-with-go/src/data_structures/linked_list"
+)
 
 func main() {
 	l := linkedlist.New()
@@ -17,4 +22,14 @@ func main() {
 	l.InsertAtHead(10)
 
 	l.Print()
+
+	numbers := []int{
+		1, 3, 6, 10, 15, 21, 28, 36,
+		45, 55, 66, 78, 91, 105, 120, 136,
+		153, 171, 190, 210, 231, 253, 276, 300,
+		325, 351, 378, 406, 435, 465, 496, 528,
+	}
+
+	fmt.Printf("Target %d was found at %d\n", 21, bs.BinarySearch(numbers, 21)) // 5
+	fmt.Printf("Target %d was found at %d\n", 91, bs.BinarySearch(numbers, 91)) // 11
 }
